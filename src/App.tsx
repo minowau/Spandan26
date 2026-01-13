e550cfed9d4504abd9b497d191e4f2ff8d8463d2
svnsaisathvik <saisathviksvn@gmail.com>
2026-01-13 23:22:45 +0530

    fixed 404 issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
 import Home from './pages/Home';
 import Sports from './pages/Sports';
 import Team from './pages/Team';
