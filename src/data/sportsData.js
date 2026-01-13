7746c3f124f15dff3bc24b841641bf91afef632a
svnsaisathvik <saisathviksvn@gmail.com>
2026-01-13 18:07:25 +0530

    added 2 links

--- src/data/sportsData.js
+++ src/data/sportsData.js
@@ -137,7 +137,7 @@ export const sportsData = [
     format: 'Knockout',
     gif_image: 'images/Gifs/Football.gif',
     registrations: [
-      { label: 'Register Here', link: 'https://forms.google.com/football' }
+      { label: 'Register Here', link: 'https://forms.gle/H3pwor28zYER23sQA' }
     ],
     // rulesLink: 'https://example.com/athletics-rules.pdf'
   },
@@ -201,7 +201,7 @@ export const sportsData = [
     teamSize: '7 players',
     format: 'League + Knockout',
     registrations: [
-      { label: 'Register Here', link: 'https://forms.google.com/throwball' }
+      { label: 'Register Here', link: 'https://forms.gle/83YBg7sniFbJizir6' }
     ],
     // rulesLink: 'https://example.com/athletics-rules.pdf'
   },
